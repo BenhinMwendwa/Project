656db21128192d2301304c4ede367395bfc23163
Benhin <benhinmwendwa7@gmail.com>
2024-03-18 10:14:38 +0300

    portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+function displayDate (){
+    document.getElementById("information").innerHTML=Date ();
+     
+}
+
+function AlertMe(){
+
+ alert("I do branding as a part time job..you can check my instagram page @Royalty Technologies  to get more information");
+}
